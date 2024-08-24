00628acca2d412e3c7aaf830d9f34c59223529df
JamesTimothyMeech <james.t.meech@gmail.com>
2024-08-24 11:00:51 +0100

    Trying simple design with eight bit output PCG that we can switch on and off

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_crispy_vga(
+module tt_um_vga_example(
   input  wire [7:0] ui_in,    // Dedicated inputs
   output wire [7:0] uo_out,   // Dedicated outputs
   input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,34 +17,36 @@ module tt_um_crispy_vga(
 );
 
   // VGA signals
-  reg hsync;
-  reg vsync;
-  reg [1:0] R;
-  reg [1:0] G;
-  reg [1:0] B;
-  reg video_active;
+  wire hsync;
+  wire vsync;
+  wire [1:0] R;
+  wire [1:0] G;
+  wire [1:0] B;
+  wire video_active;
+  wire [9:0] pix_x;
+  wire [9:0] pix_y;
 
   // TinyVGA PMOD
-  always @ (posedge clk) 
-	begin
-    hsync = ui_in[0];
-    B[0] = ui_in[1] + (B[0] + pcg_out[0]);
-    G[0] = ui_in[2] + (G[0] + pcg_out[1]);
-    R[0] = ui_in[3] + (R[0] + pcg_out[2]);
-    vsync = ui_in[4];
-    B[1] = ui_in[5] + (B[1] + pcg_out[3]);
-    G[1] = ui_in[6] + (G[1] + pcg_out[4]);
-    R[1] = ui_in[7] + (R[1] + pcg_out[5]);
-  end
-
-  assign uo_out = {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]};
+  assign uo_out = {hsync + (pcg_out[0] & ui_in[0]), B[0] + (pcg_out[1] & ui_in[1]), G[0] + (pcg_out[2] & ui_in[2]), R[0] + (pcg_out[3] & ui_in[3]), vsync + (pcg_out[4] & ui_in[4]), B[1] + (pcg_out[5] & ui_in[5]), G[1] + (pcg_out[6] & ui_in[6]), R[1] + (pcg_out[7] & ui_in[7])};
 
   // Unused outputs assigned to 0.
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // Suppress unused signals warning
-  wire _unused_ok = &{ena, uio_in};
+  wire _unused_ok = &{ena, ui_in, uio_in};
+
+  reg [9:0] counter;
+
+  hvsync_generator hvsync_gen(
+    .clk(clk),
+    .reset(~rst_n),
+    .hsync(hsync),
+    .vsync(vsync),
+    .display_on(video_active),
+    .hpos(pix_x),
+    .vpos(pix_y)
+  );
 
   reg [7:0] pcg_out = 8'h00;
   reg [7:0] xorshifted = 8'h00;
@@ -58,5 +60,19 @@ module tt_um_crispy_vga(
 		rot = state >> 3;
 		pcg_out = (xorshifted >> rot) | (xorshifted << ((-rot) & 15));
 	end
+  
+  wire [9:0] moving_x = pix_x + counter;
+
+  assign R = video_active ? {moving_x[5], pix_y[2]} : 2'b00;
+  assign G = video_active ? {moving_x[6], pix_y[2]} : 2'b00;
+  assign B = video_active ? {moving_x[7], pix_y[5]} : 2'b00;
+  
+  always @(posedge vsync) begin
+    if (~rst_n) begin
+      counter <= 0;
+    end else begin
+      counter <= counter + 1;
+    end
+  end
   
 endmodule
