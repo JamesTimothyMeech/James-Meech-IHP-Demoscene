977e978f2a3b0a88c89ba94e6a7445ec58167557
JamesTimothyMeech <james.t.meech@gmail.com>
2024-08-24 07:56:24 +0100

    Fixing multiple drivers

--- src/project.v
+++ src/project.v
@@ -23,26 +23,28 @@ module tt_um_crispy_vga(
   wire [1:0] G;
   wire [1:0] B;
   wire video_active;
-  wire [9:0] pix_x;
-  wire [9:0] pix_y;
 
   // TinyVGA PMOD
   assign hsync = ui_in[0];
-  assign B[0] = ui_in[1];
-  assign G[0] = ui_in[2];
-  assign R[0] = ui_in[3];
+  assign B[0] = ui_in[1] + (B[0] + pcg_out[0]);
+  assign G[0] = ui_in[2] + (G[0] + pcg_out[1]);
+  assign R[0] = ui_in[3] + (R[0] + pcg_out[2]);
   assign vsync = ui_in[4];
-  assign B[1] = ui_in[5];
-  assign G[1] = ui_in[6];
-  assign R[1] = ui_in[7];
+  assign B[1] = ui_in[5] + (B[1] + pcg_out[3]);
+  assign G[1] = ui_in[6] + (G[1] + pcg_out[4]);
+  assign R[1] = ui_in[7] + (R[1] + pcg_out[5]);
   assign uo_out = {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]};
+
+  assign R = video_active ? (R & pcg_out[0:1]) : 2'b00;
+  assign G = video_active ? (G & pcg_out[2:3]) : 2'b00;
+  assign B = video_active ? (B & pcg_out[4:5]) : 2'b00;
 
   // Unused outputs assigned to 0.
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // Suppress unused signals warning
-  wire _unused_ok = &{ena};
+  wire _unused_ok = &{ena, uio_in};
 
   reg [31:0] pcg_out = 32'h00000000;
   reg [31:0] xorshifted = 32'h00000000;
@@ -56,9 +58,5 @@ module tt_um_crispy_vga(
 		rot = state >> 59;
 		pcg_out = (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
 	end
-
-  assign R = video_active ? (R & pcg_out[0:1]) : 2'b00;
-  assign G = video_active ? (G & pcg_out[2:3]) : 2'b00;
-  assign B = video_active ? (B & pcg_out[4:5]) : 2'b00;
   
 endmodule
