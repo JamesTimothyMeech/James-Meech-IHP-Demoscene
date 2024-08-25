eceb634ebd822ae1a35b049fdd65373330db66d0
JamesTimothyMeech <james.t.meech@gmail.com>
2024-08-25 16:21:27 +0100

    Seeing if test passes as gate level test

--- test/test.py
+++ test/test.py
@@ -4,6 +4,7 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
+from cocotb.binary import BinaryValue
 
 
 @cocotb.test()
@@ -26,8 +27,59 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    assert dut.uo_out[0].value == 1
+    # Check bit zero of input can go high on its own
+    assert dut.uo_out.value == 0
     dut.ui_in[0].value = 1
     await ClockCycles(dut.clk, 1)
-    assert dut.uo_out[0].value == 1
+    assert dut.uo_out.value == BinaryValue("00000001")
+    dut.ui_in[0].value = 0
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 0
+    dut.ui_in[1].value = 1
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == BinaryValue("00000010")
+    dut.ui_in[1].value = 0
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 0
+    dut.ui_in[2].value = 1
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == BinaryValue("00000100")
+    dut.ui_in[2].value = 0
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 0
+    dut.ui_in[3].value = 1
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == BinaryValue("00001000")
+    dut.ui_in[3].value = 0
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 0
+    dut.ui_in[4].value = 1
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == BinaryValue("00010000")
+    dut.ui_in[4].value = 0
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 0
+    dut.ui_in[5].value = 1
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == BinaryValue("00100000")
+    dut.ui_in[5].value = 0
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 0
+    dut.ui_in[6].value = 1
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == BinaryValue("01000000")
+    dut.ui_in[6].value = 0
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 0
+    dut.ui_in[7].value = 1
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == BinaryValue("10000000")
+    dut.ui_in[7].value = 0
+    await ClockCycles(dut.clk, 1)
