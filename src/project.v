dc95ea650378cdf10465fe03490d107c0e5fa3ab
JamesTimothyMeech <james.t.meech@gmail.com>
2024-08-29 16:59:46 +0100

    Add functionality to add more programmable noise to the audio signal

--- src/project.v
+++ src/project.v
@@ -23,10 +23,12 @@ module tt_um_crispy_vga(
   wire [1:0] G;
   wire [1:0] B;
 
-  // TinyVGA PMOD
+  // TinyVGA PMOD passthrough with programmable added noise
   assign  {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]} = ui_in;
   assign uo_out = {hsync + (pcg_out[0] & uio_in[0]), B[0] + (pcg_out[1] & uio_in[1]), G[0] + (pcg_out[2] & uio_in[2]), R[0] + (pcg_out[3] & uio_in[3]), vsync + (pcg_out[4] & uio_in[4]), B[1] + (pcg_out[5] & uio_in[1]), G[1] + (pcg_out[6] & uio_in[2]), R[1] + (pcg_out[7] & uio_in[3])};
-  assign uio_out[7] = uio_in[6] + (pcg_out[7] & uio_in[5]);
+  
+  // Audio PMOD passthrough with programmable added noise
+  assign uio_out[7] = uio_in[6] + (pcg_out[0] & uio_in[0]) + (pcg_out[1] & uio_in[1]) + (pcg_out[2] & uio_in[2]) + (pcg_out[3] & uio_in[3]) + (pcg_out[4] & uio_in[4]) + (pcg_out[5] & uio_in[4]) + (pcg_out[6] & uio_in[5]) + (pcg_out[7] & uio_in[5]);
 
   // Unused outputs assigned to 0.
   assign uio_out[6] = 1'b0;
